ff97aaa8fcfd9c1975039d4525111c31384429f5
Shahlam saifi <Shahlamsaifi96@gmail.com>
2024-08-22 14:56:26 +0530

    add my project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+// import './App.css'
+import Expense from './Expense'
+
+function App() {
+ 
+
+  return (
+   
+      <>
+       <Expense />
+     
+      </>
+    
+    
+  )
+}
+
+export default App
---
+++ src/Expense.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+
+const Expense = () => {
+
+  const [input, setInput] = useState("");
+  const [amount, setAmount] = useState("")
+  const [expenses,setExpenses] = useState([])
+
+
+  const addExpense = () => {
+    if (!input || !amount) return;
+
+
+    const newExpense = {
+      id: expenses.length + 1,
+      title: input,
+      amount: amount
+    };
+    setExpenses([...expenses, newExpense])
+    setInput('')
+    setAmount('')
+  }
+
+  const deleteExpense = (id) => {
+    setExpenses(expenses.filter((expenses) => expenses.id !== id))
+  }
+
+  return (
+    <div className="container">
+      <h2>Expense Tracker</h2>
+      <div>
+        <input type="text" placeholder="Expense" value={input} onChange={(e) => setInput(e.target.value)}/>
+      </div>
+      <div>
+        <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <br/>
+        <button onClick={addExpense}>
+          Add Expense
+        </button>
+      </div>
+      <ul className="expenses-list">
+        {
+          expenses.map((expense) => (
+            <li key={expense.id}>
+              <span>{expense.title}</span>
+              <span>{expense.amount}</span>
+              <button onClick={() => deleteExpense(expense.id)}>Delete</button>
+            </li>
+          ))
+      }
+      </ul>
+    </div>
+  );
+}
+
+export default Expense;
